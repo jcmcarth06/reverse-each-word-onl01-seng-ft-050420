5ccb83cc6ac261f7ab88845ff442e053282774e7
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-08 01:42:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   words_array = sentence.split(" ")
   reverse_array = []
   reverse_array = reverse_array.collect { |i| i.reverse }
-  reverse_array.join(" ")
+  puts reverse_array.join(" ")
 
 end
